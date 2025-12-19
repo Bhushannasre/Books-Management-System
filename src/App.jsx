6117e8b66feebf7e6afc93e398524dfa9fc15b42
Bhushan Nasre <bhushannasre3@gmail.com>
2025-12-20 00:35:43 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import BookList from "./components/BookList";
 import { booksData } from "./utils/mockData";
-
+import "./index.css";
+import { useState } from "react";
 function App() {
+   const [searchText, setSearchText] = useState("");
+   const [filteredBooks, setFilteredBooks] = useState(booksData);
+  function handleSearch() {
+    const filteredBooks = booksData.filter((book) =>
+      book.title.toLowerCase().includes(searchText.toLowerCase()) ||
+      book.author.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredBooks(filteredBooks);
+    console.log("Filtered Books:", filteredBooks);
+  }
+  function clearSearch(){
+    setFilteredBooks(booksData);
+    setSearchText("");
+    window.location.reload();
+  }
   return (
     <>
-      <BookList booksDatas={booksData} />
+      <h1>Book Management System</h1>
+   <div className="search">
+  <input type="text" placeholder="Search books..."  className="search-input" onChange={e=>{setSearchText(e.target.value)}}/>
+      <button className="search-btn" onClick={handleSearch} >Search</button>
+      <button  className="search-btn" onClick={clearSearch}>Clear</button>
+   </div>
+
+      <BookList booksDatas={filteredBooks} />
     </>
   );
 }
